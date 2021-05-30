ff6b61a98a96dabbae9a61894414055c02f08e7f
sukesan000 <oyama.k.1997@gmail.com>
2021-05-30 18:07:30 +0900

    first commit

---
+++ src/main/java/com/example/phrase_game/PhraseGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.phrase_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseGameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/phrase_game/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.phrase_game.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "phrase_game";
+    }
+
+}
