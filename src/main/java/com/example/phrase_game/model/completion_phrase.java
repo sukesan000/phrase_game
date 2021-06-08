4a738af94072ffdba55b247973d3f7a18368f8c7
sukesan000 <oyama.k.1997@gmail.com>
2021-06-08 19:51:35 +0900

    modelクラス作成

---
+++ src/main/java/com/example/phrase_game/model/completion_phrase.java
@@ -0,0 +1,11 @@
+package com.example.phrase_game.model;
+
+import lombok.Data;
+
+@Data
+public class completion_phrase {
+    private String word1 = "◯◯◯";
+    private String word2 = "◯◯◯";
+    private String word3 = "◯◯◯";
+    private String word4 = "◯◯◯";
+}
